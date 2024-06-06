5863da88797c8cff3f769b6ae4b563b28bd71601
benjolyman <benjolyman@hotmail.com>
2024-06-06 19:36:37 +1000

    completed game Hangman 1.0

--- hangman-game/main.js
+++ hangman-game/main.js
@@ -1,16 +1,3 @@
-//todo
-
-//set win con (set win message)
-//set lose con (block functions when game is lost)
-//expand word bank
-//display lives left
-//create reset button
-//show hint `maybe`
-//log already selected incorrect words and show 
-
-
-
-
 //variables
 
 let winner;
@@ -30,6 +17,8 @@ const gameMsg = document.querySelector('#game-message');
 const chances = document.querySelector('#chances');
 
 const letters = document.querySelectorAll('.letters')
+
+
 //functions
 
 
@@ -41,74 +30,73 @@ const randomWord = () => {
 
 const selectedWord = randomWord().split('');
 
-
 selectedWord.forEach((element) => {
     onScreenWord.push('_');
 });
-
-console.log(selectedWord);
 
 onScreenWord.forEach((element) => {
     let li = document.createElement('li');
     li.textContent = element;
     li.classList.add('letters');
     blankLetter.appendChild(li);
-    
+
 });
+
 
 //resets game
 const resetGame = () => {
     location.reload();
 };
 
-
-const checkWin = () => {
-    if (onScreenWord.join('') === selectedWord.join('')) {
-        gameMsg.innerText = 'Nice!'
-    }   
-}
-
-const checkLoss = () => {
-    if (incorrectGuesses === 5){
-        console.log('loser');
-    }
-};
-
+//disables keyboard which is used for checkwin and checkloss
 const disableKeyboard = () => {
     keyboard.forEach((element) => {
         element.disabled = true;
     });
 }
 
+const checkWin = () => {
+    if (onScreenWord.join('') === selectedWord.join('')) {
+        gameMsg.innerText = 'Nice!'
+        disableKeyboard();
+    }
+}
+
+const checkLoss = () => {
+    if (incorrectGuesses === 6) {
+        
+        gameMsg.innerText = `The correct word was ${selectedWord.join('')}`;
+        disableKeyboard();
+    }
+};
+
+
 
 //event listeners
 
-// window.onload = () => {
-//     init();
-// }
-
+//determines whether the selected key matches the selected word or not and lets the user know whether or not the chosen letter is right or wrong
 keyboard.forEach((key) => {
     key.addEventListener('click', event => {
         let clickedLetter = event.target.innerText;
-        
+        if (key.disabled) return;
+
         if (selectedWord.includes(clickedLetter)) {
             selectedWord.forEach((element, index) => {
                 if (element === clickedLetter) {
                     onScreenWord[index] = element;
                     let li = blankLetter.querySelectorAll('li');
                     li[index].textContent = element;
+                    event.target.style.backgroundColor = 'green';
+                    event.target.style.color = 'white';
                 }
             })
-        } else if (!selectedWord.includes(clickedLetter) && incorrectGuesses < 5) {
+        } else if (!selectedWord.includes(clickedLetter) && incorrectGuesses <= 5) {
             incorrectGuesses += 1;
-            // gameMsg.innerHTML = (`You have used ${incorrectGuesses}/6 chances`);
             chances.src = `heart assets/heart ${incorrectGuesses}.png`
-            
-        } else { 
-            gameMsg.innerHTML = (`The correct word was ${selectedWord.join('')}`);
-            chances.src = `heart assets/heart 6.png`;
-            
-        };
+            event.target.style.backgroundColor = 'red';
+            event.target.style.color = 'white';
+        }
+        event.target.disabled = true;
         checkWin();
         checkLoss();
     })
